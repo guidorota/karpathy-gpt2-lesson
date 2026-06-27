dd09f7c9d22010df59cd8b5fe2aa4fbfe247823d
Guido Rota <guido.rota@gmail.com>
2026-06-27 21:43:39 +1000

    Add variable for device, add initial training dataset

--- train_gpt2.py
+++ train_gpt2.py
@@ -156,16 +156,19 @@ class GPT(nn.Module):
 num_return_sequences = 5
 max_length = 30
 
-model = GPT.from_pretrained('gpt2')
+device = "cuda"
+
+# model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
 model.eval()
-model.to("cuda")
+model.to(device)
 
 import tiktoken
-enc = tiktoken.get_encoding('gpt2')
+enc = tiktoken.get_encoding("gpt2")
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to('cuda')
+x = tokens.to(device)
 
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
