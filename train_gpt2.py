da828a5395b027fdfa5a63de5749c983fdd884bf
Guido Rota <guido.rota@gmail.com>
2026-06-29 22:06:25 +1000

    Additional optimisations, learning rate, optimizer changes

--- train_gpt2.py
+++ train_gpt2.py
@@ -184,6 +184,30 @@ class GPT(nn.Module):
 
         return model
     
+    def configure_optimizer(self, weight_decay, learning_rate):
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # Usually we only weight decay that participate in matmul, and the embeddings
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        # Common not to weight decay 1-dimension params (biases, layernorm, scales, etc.)
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        # Weight decay regularises the network by making sure that no weight
+        # grows too large, hence helps distribute probabilities more
+        # uniformely across different options
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} params")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} params")
+
+        use_fused = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8)
+        return optimizer
+    
 class DataLoaderLite:
 
     def __init__(self, B, T, device):
@@ -259,7 +283,7 @@ def get_lr(step):
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
     return min_lr + coeff * (max_lr - min_lr)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=3e-4)
 dts = []
 tpss = []
 for step in range(max_steps):
